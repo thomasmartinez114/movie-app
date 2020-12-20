abfaba3ceaaa5b1a136ee4153dd4b99615a393e3
Thomas Martinez <tmartinez114@gmail.com>
2020-12-20 09:55:26 -0500

    added MovieListHeading component -- useEffect

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -2,12 +2,14 @@ import React, { useState, useEffect } from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
 import MovieList from './components/MovieList';
+import MovieListHeading from './components/MovieListHeading';
 
 const App = () => {
   const [movies, setmovies] = useState([]);
+  const [searchValue, setSearchValue] = useState('');
 
   const getMovieRequest = async () => {
-    const url = "http://www.omdbapi.com/?s=home alone&apikey=fe627a67"
+    const url = "http://www.omdbapi.com/?s=star wars&apikey=fe627a67"
 
     const response = await fetch(url);
     const responseJson = await response.json(); //convert http request to json
@@ -18,10 +20,13 @@ const App = () => {
 
   useEffect(() => {
     getMovieRequest();
-  }, []);
+  }, []); // gets called when page loads only
 
   return (
   <div className="container-fluid movie-app">
+    <div className='row'>
+      <MovieListHeading heading='Movies'/>
+    </div>
     <div className="row">
       <MovieList movies = {movies} />
     </div>
