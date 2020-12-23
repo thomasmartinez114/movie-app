233286209d2418c621cae433fa5bec816ab72cb0
Thomas Martinez <tmartinez114@gmail.com>
2020-12-22 22:52:35 -0500

    added Favorite Icon to overlay

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import MovieList from './components/MovieList';
 import MovieListHeading from './components/MovieListHeading';
 import SearchBox from './components/SearchBox';
+import AddFavorites from './components/AddFavorites';
 
 const App = () => {
   const [movies, setmovies] = useState([]);
@@ -32,7 +33,7 @@ const App = () => {
       <SearchBox searchValue={searchValue} setSearchValue={setSearchValue}/>
     </div>
     <div className="row">
-      <MovieList movies = {movies} />
+      <MovieList movies = {movies} favoriteComponent={AddFavorites}/>
     </div>
   </div>
   );
